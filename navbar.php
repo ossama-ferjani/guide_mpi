1e80524aa019e7d23a2564ad87d6bf8571376e29
ossama <ossama.ferjani@insat.ucar.tn>
2023-04-20 12:56:41 +0200

    nav/head

---
+++ navbar.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="home.html"><img id="logo_insat" src="assets/img/no-bg_logo.png"
+                        alt="logo insat"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa-solid fa-bars fa-xl" style="color: #ffffff;"></i>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a id="home" class="nav-link" href="home.html">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="home" class="nav-link" href="home.html">Resources et Formation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Activités</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">A propos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contacter-Nous</a>
+                        </li>
+
+
+                    </ul>
+
+                    <ul class="navbar-nav list-inline">
+                        <li>
+                            <a id="hidden-sign-up" class="btn btn-primary" href="#" role="button">S'inscrire</a>
+
+                        </li>
+                        <li>
+                            <span id="bar"></span>
+                        </li>
+
+                        <li>
+                            <a id="sign-in" class="btn btn-primary" href="#" role="button">Se connecter</a>
+
+                        </li>
+
+
+                </div>
+            </div>
+
+        </nav>
