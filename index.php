a9870c789fac3622b345d08a5eba2b711d297696
Mohamed Aziz Bchini <114874129+mo-hamedaziz@users.noreply.github.com>
2023-05-01 17:10:38 +0100

    added seperated navbar+home page v3.0

--- index.php
+++ index.php
@@ -11,14 +11,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/home.css">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="css/navbar.css">
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
-
+    <script src="js/home.js" defer></script>
 
 
 </head>
@@ -28,55 +27,10 @@
 
     <header>
         <!--<img id="landing" src="img/landing.png" alt="Landing">-->
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="home.html"><img id="logo_insat" src="assets/img/no-bg_logo.png"
-                        alt="logo insat"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa-solid fa-bars fa-xl" style="color: #ffffff;"></i>
-                </button>
-                <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a id="home" class="nav-link" href="home.html">Accueil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a id="home" class="nav-link" href="home.html">Resources et Formation</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Activités</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">A propos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contacter-Nous</a>
-                        </li>
+        <?php
+            include('navbar.php');
+        ?>
 
-
-                    </ul>
-
-                    <ul class="navbar-nav list-inline">
-                        <li>
-                            <a id="hidden-sign-up" class="btn btn-primary" href="#" role="button">S'inscrire</a>
-
-                        </li>
-                        <li>
-                            <span id="bar"></span>
-                        </li>
-
-                        <li>
-                            <a id="sign-in" class="btn btn-primary" href="#" role="button">Se connecter</a>
-
-                        </li>
-
-
-                </div>
-            </div>
-
-        </nav>
         <br><br><br><br><br><br><br>
         <div id="landing-txt">
             <h1>INSAT</h2>
@@ -92,14 +46,13 @@
         <br><br>
         <div id="explore">
             <a href="#">
-                Explorer INSAT <span style="font-weight: bold;font-size: 1.5em;position: relative; top: 10px;"
-                    class="material-symbols-outlined">
+                Explorer INSAT <span style="font-weight: bold;font-size: 1.5em;position: relative; top: 10px;" class="material-symbols-outlined">
                     expand_more
                 </span>
             </a>
     </header>
     <section>
-        <div class="ctn-2">
+        <div id='a_propos_ctn' class="ctn-2">
             <div class="row">
                 <div class="row col-lg-8 col-sm-4">
                     <div class="col-lg-6 col-md-3 col-sm-3">
@@ -124,9 +77,8 @@
 
                     </div>
                     <div class="col-lg-6 col-md-3 col-sm-3">
-                        <h4 class="p-head">C'est quoi la filière MPI ?</h4>
-                        <p class="p-sub">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto unde
-                            consectetur
+                        <h4 class="p-head">La vie associative à l'INSAT</h4>
+                        <p class="p-sub">La vie associative à l'INSAT est foisonnante, et son dynamisme anime une vie étudiante riche et variée tout au long de l’année universitaire.
                         </p>
 
                     </div>
@@ -159,12 +111,9 @@
                 <br>
                 <div id="carouselExampleIndicators" class="carousel slide">
                     <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                            class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                            aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                            aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
                     </div>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -177,20 +126,79 @@
                             <img src="assets/img/landing.png" class="d-block w-100" alt="...">
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="prev">
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
-                        data-bs-slide="next">
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
 
             </div>
-            <br>
+            <br><br><br>
+            <div class="row">
+                <div class="col-lg-12" style="text-align: center;">
+                    <h2>Explorer l'INSAT</h2>
+                </div>
+                <ul id="sections_ctn">
+                    <li class="col-lg-3 col-md-6 col-sm-12 sections" style="text-align: center;">
+                        <a href="">
+                            <img src="assets/img/calculator.jpg" alt="Calculator" style="width:50%">
+                            <br>
+                            <span>
+                                Calculer votre moyenne <br> Moyenne | Score
+                            </span>
+                        </a>
+                    </li>
+                    <li class="col-lg-3 col-md-6 col-sm-12 sections" style="text-align: center;">
+                        <a href="">
+                            <img src="assets/img/forum.jpg" alt="Forum" style="width:85%">
+                            <br>
+                            <span>
+                                Le forum <br> Q&A
+                            </span>
+                        </a>
+                    </li>
+                    <li class="col-lg-3 col-md-6 col-sm-12 sections" style="text-align: center;">
+                        <a href="">
+                            <img src="assets/img/ressources.jpg" alt="Resources" style="width:60%;padding:10px;">
+                            <br>
+                            <span>
+                                Les ressources
+                            </span>
+                        </a>
+                    </li>
+                    <li class="col-lg-3 col-md-6 col-sm-12 sections" style="text-align: center;">
+                        <a href="">
+                            <img src="assets/img/paths.jpg" alt="Calculator" style="width:60%">
+                            <br>
+                            <span>
+                                Presentation des Filieres
+                            </span>
+                        </a>
+                    </li>
+                    <li class="col-lg-3 col-md-6 col-sm-12 sections" style="text-align: center;">
+                        <a href="">
+                            <img src="assets/img/vie_asso.jpg" alt="Calculator" style="width:75%">
+                            <br>
+                            <span>
+                                La vie associative <br>dans l'INSAT
+                            </span>
+                        </a>
+                    </li>
+                    <li class="col-lg-3 col-md-6 col-sm-12 sections" style="text-align: center;">
+                        <a href="">
+                            <img src="assets/img/convention.jpg" alt="Calculator" style="width:55%;">
+                            <br>
+                            <span>
+                                Nos conventions
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
 
 
@@ -218,8 +226,7 @@
                 </div>
                 <div class="col-lg-4 col-sm-4  ctn-3">
                     <p>
-                        <span style="font-weight: bold;">Tél:</span> <br> 71 703 829 - 71 703 929 <br><br> <span
-                            style="font-weight: bold;">Fax:</span> <br>71 704 329
+                        <span style="font-weight: bold;">Tél:</span> <br> 71 703 829 - 71 703 929 <br><br> <span style="font-weight: bold;">Fax:</span> <br>71 704 329
                     </p>
 
                 </div>
@@ -231,10 +238,8 @@
 
                     <a href="http://www.insat.rnu.tn/"><i class="fa-solid fa-globe fa-sm" style="color: #ffffff;"></i>
                         Website <br></a>
-                    <a href="https://www.facebook.com/insat.rnu.tn"><span class="fa-brands fa-square-facebook fa-sm"
-                            style="color: #000000;"></span> Facebook </a><br>
-                    <a href="https://www.linkedin.com/school/national-institute-of-applied-science-and-technology/"><span
-                            class="fa-brands fa-linkedin fa-sm" style="color: #000000;"></span> LinkedIn </a><br>
+                    <a href="https://www.facebook.com/insat.rnu.tn"><span class="fa-brands fa-square-facebook fa-sm" style="color: #000000;"></span> Facebook </a><br>
+                    <a href="https://www.linkedin.com/school/national-institute-of-applied-science-and-technology/"><span class="fa-brands fa-linkedin fa-sm" style="color: #000000;"></span> LinkedIn </a><br>
                     <p><i class="fa-sharp fa-solid fa-envelope fa-sm" style="color: #ffffff;"></i> contact@insat.ucar.tn
                     </p>
 
@@ -246,7 +251,7 @@
 
         </div>
     </footer>
-    <script src="js/home.js"></script>
+
 </body>
 
 
---
+++ nav0.php
@@ -0,0 +1,61 @@
+<script src="js/nav0.js" defer></script>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="home.html"><img id="logo_insat" src="assets/img/no-bg_logo.png" alt="logo insat"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars fa-xl" style="color: #ffffff;"></i>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a id="home" class="nav-link" href="#">Accueil</a>
+                </li>
+                <!--<li class="nav-item">
+                    <a id="home" class="nav-link" href="home.html">Resources et Formation</a>
+                </li>-->
+                <!-- Example single danger button -->
+                <li class="nav-item">
+                    <div class="btn-group">
+                        <button class="nav-link" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            Resources et Formation<i class="fa-solid fa-caret-down fa-sm" style="color: #ffffff;"></i>
+                        </button>
+                        <ul class="dropdown-menu animate slideIn">
+                            <li><a class="dropdown-item" href="#">Resources</a></li>
+                            <li><a class="dropdown-item" href="#">Forum</a></li>
+                            <li><a class="dropdown-item" href="#">Outils</a></li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Activités</a>
+                </li>
+                <li id="a_propos" class="nav-item">
+                    <a class="nav-link" href="#">A propos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacter-Nous</a>
+                </li>
+
+
+            </ul>
+
+            <ul class="navbar-nav list-inline">
+                <li>
+                    <a id="hidden-sign-up" class="btn btn-primary" href="/SignUp.php" role="button">S'inscrire</a>
+
+                </li>
+                <li>
+                    <span id="bar"></span>
+                </li>
+
+                <li>
+                    <a id="sign-in" class="btn btn-primary" href="./SignIn.php" role="button">Se connecter</a>
+
+                </li>
+
+
+        </div>
+    </div>
+
+</nav>
