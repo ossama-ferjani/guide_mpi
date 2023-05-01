073ad40a516887743077db82109309fc42b979dc
ines-el-bour <ines.elbour@insat.ucar.tn>
2023-05-01 16:46:59 +0100

    ines pushes now

--- studentcard.js
+++ studentcard.js
@@ -1,6 +1,6 @@
-// add an event listener to the "Editer" button
+
 document.querySelector('.edit-button').addEventListener('click', function() {
-    // get the values entered by the user in the form
+
     const nom = document.querySelector('input[placeholder="Nom"]').value;
     const prenom = document.querySelector('input[placeholder="Prénom"]').value;
     const email = document.querySelector('input[placeholder="entrer votre email universiataire"]').value;
@@ -8,7 +8,7 @@ document.querySelector('.edit-button').addEventListener('click', function() {
     const niveau = document.querySelector('#niveau_etudes').value;
     const filiere = document.querySelector('#filieres').value;
 
-    // update the content in "Informations Personnelles" using the new values
+
     document.querySelector('#tab1 h4:nth-of-type(1)').textContent = 'Nom: ' + nom +prenom
     ;
     document.querySelector('#tab1 h4:nth-of-type(3)').textContent = 'Email: ' + email;
