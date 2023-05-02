bd0a4370a65938d8c345203602debbf70c6d2e21
Imtiez Mrad <imtiez.mrad@insat.ucar.tn>
2023-05-02 15:11:45 +0100

    lamya push action database

--- signUp.php
+++ signUp.php
@@ -1,19 +1,99 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-    <title>
-        <?php
-        if (isset($pageTitle)) {
-            echo $pageTitle;
-        } else {
-            echo "RT2 G3";
-        }
-        ?>
-    </title>
-</head>
-<body>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sign Up</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
+    <link rel="icon" href="assets/icons/logoinsat-cropped.ico">
+    
+
+
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row justify-content-center align-items-center">
+        <div class="col-md-6">
+          <div class="form-container">
+            <h2 id="title">S'inscrire</h2>
+     
+            
+            <div class="tab-content mt-3">
+              <div id="signUp">
+                <form>
+                  <div class="form-group">
+                    <label for="prenom" class="input-label">Prénom:</label>
+                    <input type="text" class="form-control" id="prenom" aria-describedby="prenomHelp" placeholder="Prénom de l'étuidant" required>
+                    <span id="SNerror-message"></span>
+                  </div>
+                  <div class="form-group">
+                    <label for="nom" class="input-label">Nom:</label>
+                    <input type="text" class="form-control" id="nom" aria-describedby="nomHelp" placeholder="Nom de l'étuidant" required>
+                    <span id="NAMEerror-message"></span>
+                  </div>
+               
+                  <div class="form-group">
+                    <label for="tel" class="input-label">Numéro d'inscription:</label>
+                    <input type="tel" class="form-control" id="tel" aria-describedby="telHelp" placeholder="Numéro d'inscription">
+                    <span id="telerror-message"></span>
+                  </div>
+                  <div class="form-row align-items-center">
+                    <div class="col-auto">
+                        <label for="filiere">Filière:</label>
+                        <select class="form-control form-control-sm" id="filiere" name="filiere">
+                            <option selected disabled>Choisir...</option>
+                            <option value="MPI">MPI</option>
+                            <option value="RT">RT</option>
+                            <option value="GL">GL</option>
+                            <option value="IIA">IIA</option>
+                            <option value="IMI">IMI</option>
+                        </select>
+                    </div>
+                    <div class="col-auto">
+                        <label for="niveau">Niveau:</label>
+                        <select class="form-control form-control-sm" id="niveau" name="niveau">
+                            <option value="choisir..." disabled selected>Choisir...</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                    </div>
+                </div>
+ 
+       
+             
+
+                  <div class="form-group">
+                    <br><label for="email" class="input-label">Email universitaire:</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="✉  Email universitare" required>
+                    <span id="mailerror-message"></span>
+                  </div>
+                  <div class="form-group">
+                    <label for="password" class="input-label">Mot de Passe:</label>
+                    
+                    <input type="password" class="form-control" id="password" placeholder="Mot de passe (minimum 8 caractéres)" required>
+                    <span id="passerror-message"></span>
+                  </div>
+                  
+                  <div class="form-group">
+                    <label for="passwordConfirm">Confirmer votre mot de passe:</label>
+                    <input type="password" class="form-control" id="passwordConfirm" placeholder="Confirmer votre mot de passe" required>
+                    <span id="passconferror-message"></span></div>
+                  <div style="text-align: center;">
+                  <button type="submit" id="submit" class="custom-button">Inscription</button><br><br>
+                  <small id="emailHelp2" class="form-text text-muted">Vouz avez déjà un compte ? <a href="SignIn.html">  Connectez vous</a></small>
+                </form>
+
+              </div>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="js/script2.js"></script>
+  </body>
+</html>
