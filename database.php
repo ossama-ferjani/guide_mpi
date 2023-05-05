d7635e141b1bdede7f8e6f74b7c00bc115582c81
ossama <ossama.ferjani@insat.ucar.tn>
2023-05-05 12:29:17 +0200

    vbc

--- database.php
+++ database.php
@@ -4,7 +4,7 @@ $dest = 'mysql:host=localhost;dbname=guide_mpi_db';
 $user = 'root';
 $pass = '';
 try {
-    session_start();
+    
     $cnx= new PDO($dest, $user, $pass);
     $cnx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(Exception $e){
