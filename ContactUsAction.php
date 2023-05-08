a9bf35623186dd974b7dfb18e41721d3dc209c27
Imtiez Mrad <imtiez.mrad@insat.ucar.tn>
2023-05-08 13:45:55 +0100

    imt

--- ContactUsAction.php
+++ ContactUsAction.php
@@ -2,24 +2,23 @@
 include 'securityAction';
 include 'database.php';
 if(isset($_POST["submit"])) {
-    $name = $_POST["Titre"];
-    $comment = $_POST["comment"];
+    $tire = htmlspecialchars($_POST["Titre"]);
+    $comment = htmlspecialchars($_POST["comment"]);
     $numInscri=$_SESSION["numInscri"];
 
 
     $stmt = $cnx->prepare("INSERT INTO feedback (contenu, Titre,NumInscri) VALUES ( ?, ?,?)");
     $stmt->bindParam(1, $comment);
-    $stmt->bindParam(2, $name);   
+    $stmt->bindParam(2, $titre);   
     $stmt->bindParam(3, $numInscri); 
 
     try {
         $stmt->execute();
         $cnx = null;
-        header('location: ContactUs.html?success=true');
+        header('location: ContactUs.php?success=true');//nbadil lurl bich nijm naffichi il popup ken il insertion fil database tinja7
     } catch(PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
     $cnx = null;
 }
-//header('location: ContactUs.php');
 ?>
