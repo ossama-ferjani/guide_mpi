ff1ae6c1668dde3096eceaf08c7005de8bb74380
Imtiez Mrad <imtiez.mrad@insat.ucar.tn>
2023-05-08 13:12:40 +0100

    do

--- publishQuestionAction.php
+++ publishQuestionAction.php
@@ -9,18 +9,18 @@ if(isset($_POST['validate'])){
         
         //Les données de la question
         $question_title = htmlspecialchars($_POST['title']);
-        $question_description = nl2br(htmlspecialchars($_POST['description']));
+       // $question_description = nl2br(htmlspecialchars($_POST['description']));
         $question_content = nl2br(htmlspecialchars($_POST['content']));
         $question_date = date('d/m/Y');
         //$question_id_author = $_SESSION['id'];
        // $question_pseudo_author = $_SESSION['pseudo'];
 
         //Insérer la question sur la question
-        $insertQuestionOnWebsite = $database->prepare('INSERT INTO questions(titre, description, contenu, id_auteur, pseudo_auteur, date_publication)VALUES(?, ?, ?, ?, ?, ?)');
+        $insertQuestionOnWebsite = $database->prepare('INSERT INTO forumquestion(titre, contenu, Poster, TempsPublication)VALUES(?, ?,  ?, ?)');
         $insertQuestionOnWebsite->execute(
             array(
                 $question_title, 
-                $question_description, 
+               // $question_description, 
                 $question_content, 
                 $question_id_author, 
                 $question_pseudo_author, 
