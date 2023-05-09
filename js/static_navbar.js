4c68bf1eff55c24bae4cf54e41a057bf29524bda
Imtiez Mrad <imtiez.mrad@insat.ucar.tn>
2023-05-09 10:13:23 +0100

    imtiez

---
+++ js/static_navbar.js
@@ -0,0 +1,14 @@
+var isLogged=true;
+
+if(isLogged){
+    document.getElementById('logged-out-nav').style.display="none";
+    document.getElementById('logged-in-nav').style.display="block";
+}
+
+else{
+    document.getElementById('logged-out-nav').style.display="block";
+    document.getElementById('logged-in-nav').style.display="none";
+}
+
+
+
